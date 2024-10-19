15cc21ed9cf1f4b8bf4745b8fe632c0d86670f47
Jailson Junior <jailsonjunior210@gmail.com>
2024-10-19 17:06:44 -0300

    html e css

--- a3distribuidos/src/Pages/PagesUser/ToMark/ToMark.jsx
+++ a3distribuidos/src/Pages/PagesUser/ToMark/ToMark.jsx
@@ -1,18 +1,58 @@
-import styleToMark  from "./ToMark.module.css";
+import styleToMark from "./ToMark.module.css";
 
 
 export default function name() {
 
   return (
-    <div>
-      <header className="header">
-        <img src="#" alt="#" />
-      </header>
+    <div className={styleToMark.container}>
+
+      <div className={styleToMark.h2}>
+        <h2>Agendar Consultas</h2>
+      </div>
+
+      <div>
+        <form className={styleToMark.box_container}>
+
+           <div className={styleToMark.dhm}>
+            <div className={styleToMark.calendar}>
+            <label htmlFor="calendario"></label>
+            <input type= "date" name="calendario"/> 
+          </div>
+
+          <div className={styleToMark.time}>
+          <label htmlFor="hora"></label>
+          <input type="time" name="hora"/>
+          </div>
+
+          <div className={styleToMark.list}>
+           <label htmlFor="Medicos"></label>
+            <select name="Medicos" id="bar">
+              <option value="aleatorio"></option>
+              <option value="aleatorio"></option>
+              <option value="aleatorio"></option>
+            </select>
+    
+          </div>
+
+          <div className={styleToMark.button}>
+            <button>Marcar</button>
+          </div>
+
+          
+
+        
+           </div>
+          </form>
+      </div>
     </div>
 
-    <div> 
-      <h1>Agendar Consultas</h1>
-    </div>
-  )
+
+
+
+  );
+
+    
+    
   
+ 
 }
