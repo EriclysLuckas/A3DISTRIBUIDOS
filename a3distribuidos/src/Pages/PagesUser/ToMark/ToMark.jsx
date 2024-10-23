5f22b18a632e3faa11643a5ccf03b18152eaee6f
Ericlys <107273056+EriclysLuckas@users.noreply.github.com>
2024-10-23 13:18:12 -0300

    att

--- a3distribuidos/src/Pages/PagesUser/ToMark/ToMark.jsx
+++ a3distribuidos/src/Pages/PagesUser/ToMark/ToMark.jsx
@@ -4,15 +4,15 @@ import styleToMark from "./ToMark.module.css";
 export default function name() {
 
   return (
-    <div className={styleToMark.container}>
+    <div className={styleToMark.Sectioncontainer}>
 
-      <div className={styleToMark.h2}>
-        <h2>Agendar Consultas</h2>
-      </div>
+    
 
       <div className={styleToMark.container}>
         <form className={styleToMark.box_container}>
-
+        <div className={styleToMark.h2}>
+        <h2>Agendar Consultas</h2>
+      </div>
            <div className={styleToMark.dhm}>
             <div className={styleToMark.bar}>
 
