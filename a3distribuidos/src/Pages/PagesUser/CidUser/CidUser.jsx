4e62daa1b41ad787c7d5eb3c7fd881ab71434031
Ericlys <107273056+EriclysLuckas@users.noreply.github.com>
2024-10-23 17:19:15 -0300

    Update CidUser.jsx

--- a3distribuidos/src/Pages/PagesUser/CidUser/CidUser.jsx
+++ a3distribuidos/src/Pages/PagesUser/CidUser/CidUser.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { Link } from "react-router-dom"; // Importando Link para navegação
 import styleCidUser from "./CidUser.module.css";
 
