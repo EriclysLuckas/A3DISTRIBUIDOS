cf7440d02434888377421d297b866ace9ef4c0f6
Jailson Junior <jailsonjunior210@gmail.com>
2024-10-27 14:29:36 -0300

    CSS

--- a3distribuidos/src/Pages/PagesUser/CadastroPac/CadastroPac.jsx
+++ a3distribuidos/src/Pages/PagesUser/CadastroPac/CadastroPac.jsx
@@ -5,7 +5,7 @@ export default function CadastroPac() {
   return(
     <div className={styleCadastroPac.Selectioncontainer}>
 
-      <form className={styleCadastroPac.box_cotainer}>
+      <form className={styleCadastroPac.box_container}>
         <div className={styleCadastroPac.h2}>
             <h2>Cria conta</h2>
         </div>
@@ -14,30 +14,30 @@ export default function CadastroPac() {
 
                 <div className={styleCadastroPac.name}>
                   <label htmlFor="Nome"></label>
-                  <input type="text" name="nome" placeholder="Nome Completo"/>
+                  <input type="text" name="nome" placeholder="Nome Completo" required/>
                 </div>
                 
                 <div className={styleCadastroPac.email}>
                   <label htmlFor="Email"></label>
-                  <input type="email" name="email" placeholder="Email"/>
+                  <input type="email" name="email" placeholder="Email" required/>
                 </div>
 
                 <div className={styleCadastroPac.cpf}>
                   <label htmlFor="CPF"></label>
-                  <input type="text" name="cpf" maxLength={14} placeholder="000.000.000-00" onInput={this}/>
+                  <input type="text" name="cpf" maxLength={14} placeholder="000.000.000-00" onInput={this} required/>
                 </div>
 
                 <div className={styleCadastroPac.number}>
                   <label htmlFor="Telefone"></label>
-                  <input type="number" name="numero"  placeholder="Telefone"/>
+                  <input type="number" name="numero"  placeholder="Telefone" required/>
                 </div>
 
                 <div className={styleCadastroPac.date}>
                   <label htmlFor="Data de Nascimento"></label>
-                  <input type="text" name="Data de Nascimento" placeholder="Data de Nascimento"/>
+                  <input type="text" name="Data de Nascimento" placeholder="Data de Nascimento" required/>
                 </div>
 
-                <div className="">
+                <div className={styleCadastroPac.button}>
                   <button className={styleCadastroPac.btn}>Cadastrar</button>
                 </div>
               </div>
