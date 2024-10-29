390b804871de3d8447f23a05df714004b5f96004
Caio Alex <caiodonato71@gmail.com>
2024-10-29 10:03:44 -0300

    Update MyQueriesUser.jsx

--- a3distribuidos/src/Pages/PagesUser/MyQueriesUser/MyQueriesUser.jsx
+++ a3distribuidos/src/Pages/PagesUser/MyQueriesUser/MyQueriesUser.jsx
@@ -4,7 +4,7 @@ export default function MyQueriesUser() {
   const daysOfWeek = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
 
   const appointments = [
-    { dayIndex: 0, doctor: "Dr Péricles Silva", crm: "CRM: 123456/7", time: "09:00" },
+    { dayIndex: 0, doctor: "Dr Péricles", crm: "CRM: 123456/7", time: "09:00" },
     { dayIndex: 0, doctor: "Dr Ana Silva", crm: "CRM: 765432/1", time: "11:00" },
     { dayIndex: 0, doctor: "Dr Kuririn", crm: "CRM: 453535/4", time: "14:00" },
     { dayIndex: 1, doctor: "Dr João Pereira", crm: "CRM: 987654/3", time: "14:00" },
