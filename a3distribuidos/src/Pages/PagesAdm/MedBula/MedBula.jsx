ee53ec3fc5b4156b73bd1924a61a673caaebdcb3
Jailson Junior <jailsonjunior210@gmail.com>
2024-11-26 09:16:42 -0300

    html

--- a3distribuidos/src/Pages/PagesAdm/MedBula/MedBula.jsx
+++ a3distribuidos/src/Pages/PagesAdm/MedBula/MedBula.jsx
@@ -2,7 +2,31 @@ import styleMedBula from "./MedBula.module.css";
 
 export default function MedBula() {
   return(
-    <p>Tela de Bulas Medicos</p>
+    <div className={styleMedBula.container}>
+      <form className={styleMedBula.box_container}>
+        <div className={styleMedBula.h1}>
+          <h1>Bulas</h1>
+        </div>
+
+        <div className={styleMedBula.search}>
+          <input type="text" name="Buscar" placeholder="Buscar" />
+        </div>
+
+        <div className={styleMedBula.list}>
+          <label htmlFor="">Categoria</label>
+          <select name="categoria" id="categoria" placeholder="Categoria">
+            <option value="">Não a Categoria</option>
+          </select>
+        </div>
+
+        <div className={styleMedBula.button}>
+          <button type="submit" className={styleMedBula.btn}>
+            Buscar
+          </button>
+        </div>
+
+      </form>
+    </div>
   )
   
 }
